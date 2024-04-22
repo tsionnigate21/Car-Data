519d3a8ddbdfa613e3691b18355f774849551a60
LoJoSho <LoJoShogaming@gmail.com>
2024-04-22 11:33:35 -0500

    feat: clean data into one data frame (initial)

--- CarData.R
+++ CarData.R
@@ -2,43 +2,127 @@ library(ggplot2)
 library(shiny)
 library(DT)
 library(readxl)
+library(dplyr)
 
 #setwd('C:/Users/stick/Documents/GitHub/Car-Data')
 dir.create('data')
-
-df <- data.frame()
 
 capitalize_words <- function(word_list) {
   sapply(word_list, function(word) {
     paste(toupper(substring(word, 1, 1)), substring(word, 2), sep = "")
   })
 }
-download <- function(name) {
-  url <- "https://github.com/hannahmaurer/Car-Data/raw/main/"
-  download.file(paste0(url, name), paste0("data/", name), quiet = TRUE)
+
+combine_dataframes <- function(df1, df2) {
+  # Identify the new columns in df2
+  new_columns <- setdiff(names(df2), names(df1))
+  
+  # Rename columns of df2 to match df1
+  colnames(df2) <- names(df1)
+  
+  # Add the new columns to df1
+  for (col in new_columns) {
+    df1[[col]] <- NA
+  }
+  
+  # Append the rows of df2 to df1
+  combined_df <- bind_rows(df1, df2)
+  
+  return(combined_df)
 }
 
+convert_percentage_to_time <- function(time_percentage) {
+  if (is.null(time_percentage)) return(time_percentage)
+  if (!grepl(":", time_percentage)) {
+    time_percentage <- as.numeric(time_percentage)
+    hour <- floor(time_percentage * 24)
+    minute <- round((time_percentage * 24 - hour) * 60)
+    if (hour < 8) {
+      hour <- hour + 12
+    }
+    time <- sprintf("%02d:%02d:00", hour, minute)
+  } else {
+    if (grepl("PM", time_percentage, fixed = TRUE)) {
+      time <- gsub(" PM", "", time_percentage)
+      if (time != "12:00") {
+        hour <- as.numeric(strsplit(time, ":")[[1]][1]) + 12
+        if (hour == 24) hour <- 0
+        time <- sprintf("%02d:%s:00", hour, strsplit(time, ":")[[1]][2])
+      }
+    } else if (grepl("AM", time_percentage, fixed = TRUE)) {
+      time <- gsub(" AM", "", time_percentage)
+      if (time == "12:00") {
+        time <- "00:00:00"
+      } else if (as.numeric(strsplit(time, ":")[[1]][1]) < 8) {
+        hour <- as.numeric(strsplit(time, ":")[[1]][1]) + 12
+        time <- sprintf("%02d:%s:00", hour, strsplit(time, ":")[[1]][2])
+      }
+    } else {
+      time <- time_percentage
+      hour <- as.numeric(strsplit(time, ":")[[1]][1])
+      if (hour < 8) {
+        hour <- hour + 12
+      }
+      time <- sprintf("%02d:%s:00", hour, strsplit(time, ":")[[1]][2])
+    }
+  }
+  return(time)
+}
+
+# Get group data
+group_1 <- read_excel('data/Car_Data.xlsx', .name_repair = 'universal', col_types = c("text"))
+group_2 <- read_excel('data/Car.xlsx', .name_repair = 'universal', skip = 1, col_types = c("text"), col_names = c("Speed", "Orange.Light", "Color", "Manufacturer", "Type", "Day", "Time", "Weather", "Temperature", "Name"))
+# Relook over group 3 (it's importing wierdly)
+group_3 <- read_excel('data/counting_cars.xlsx', .name_repair = 'universal', col_types = c("text"), skip = 1, col_names = c("Date", "Speed", "Time", "Temperature", "Weather", "V1", "V2", "V3", "V4", "V5", "V6"))
+group_4 <- read.csv('data/IRL_Car_Data.csv', colClasses = "character", col.names = c("Speed", "Time", "Temperature", "Weather", "Day", "Name"))
+abba_data <- read.csv('data/MergedCarData.csv', stringsAsFactors = FALSE, colClasses = "character")
+group_6 <- read_excel('data/Speed analyst 332 Car Data.xlsx', .name_repair = 'universal', skip = 1, col_types = c("text"), col_names = c("Name", "Date", "Speed", "Time", "Type", "Orange.Light", "Temperature", "Weather"))
+group_7 <- read.csv('data/UpdatedCarTracking.csv', stringsAsFactors = FALSE, colClasses = "character", col.names = c("CarNumber", "Time", "Temperature", "Type", "Speed", "Name"))
+
+df <- data.frame()
 # ABBA Data
-download("MergedCarData.csv")
-abba_data <- read.csv('data/MergedCarData.csv')
-df <- rbind(df, abba_data)
+df <- bind_rows(df, abba_data)
+# Group 1 
+df <- bind_rows(df, group_1)
+df <- bind_rows(df, group_2)
+df <- bind_rows(df, group_3)
+df <- bind_rows(df, group_4)
+df <- bind_rows(df, group_6)
+df <- bind_rows(df, group_7)
+
+# Drop any useless to us columns
+columns_to_remove <- c("CarNumber", "V1", "V2", "V3", "V4", "V5", "V6")
+df <- df[, !names(df) %in% columns_to_remove]
+
+df$Time <- sapply(df$Time, convert_percentage_to_time)
 
 # Assign Values
 df$Date <- as.Date(df$Date)
-df$Time <- as.Date(df$Date)
 df$Speed <- as.numeric(df$Speed)
 df$Orange.Light <- as.logical(df$Orange.Light)
 df$Temperature <- as.numeric(df$Temperature)
 
 # Clean all the data
 df$State <- replace(df$State, df$State == 'IO', 'IA') # Fix IA being IO
+df$Weather <- replace(df$Weather, df$Weather %in% c('Clear skies, sundown', 'Sunny, clear skies'), 'Sunny') # Keep it consistent 
 df$Weather <- capitalize_words(df$Weather) # Make first letter capitalize
-df$Time <- format(df$Time, "%H:%M:%S")
+
+# Get column names for dropdown menus
+column_names <- colnames(df)
 
 ui <- fluidPage(
   
   # Application title
   titlePanel("Speed Analysis"),
+  
+  fluidRow(
+    column(2,
+           selectInput("X", "Choose X", column_names, column_names[1]),
+           selectInput("Y", "Choose Y", column_names, column_names[3]),
+           selectInput("Splitby", "Split By", column_names, column_names[3])),
+    column(4, plotOutput("plot_01")),
+    column(6, DT::dataTableOutput("table_01", width = "100%"))
+  ),
   
   # Output: Results displayed in main panel
   mainPanel(
@@ -53,6 +137,7 @@ ui <- fluidPage(
 
 # Define server logic
 server <- function(input, output) {
+  
   calculate_stats <- function(data) {
     speed <- data$Speed
     min_val <- min(speed)
@@ -61,6 +146,12 @@ server <- function(input, output) {
     mean_val <- round(mean(speed), digits = 0)  # Round mean to whole number
     return(list(min = min_val, max = max_val, median = median_val, mean = mean_val, speeds = speed))
   }
+  
+  # Render the ggplot plot
+  output$plot_01 <- renderPlot({
+    ggplot(df, aes_string(x = input$X, y = input$Y, colour = input$Splitby)) +
+      geom_point()
+  })
   
   # Calculate statistics and render outputs
   output$min <- renderPrint({ paste("Minimum Speed:", calculate_stats(df)$min) })
@@ -71,6 +162,8 @@ server <- function(input, output) {
     speeds <- calculate_stats(df)$speeds
     hist(speeds, main = "Distribution of Speeds", xlab = "Speed", ylab = "Frequency", col = "skyblue")
   })
+  output$table_01 <- DT::renderDataTable(df[, c(input$X, input$Y, input$Splitby)], 
+                                         options = list(pageLength = 4))
 }
 
 # Run the application
